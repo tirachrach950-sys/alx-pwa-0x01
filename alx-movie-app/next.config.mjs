498aed0ffced45fe3ad0f3a6dd977553f2eb4122
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-15 02:30:51 +0100

    Update next.config.mjs

--- alx-movie-app/next.config.mjs
+++ alx-movie-app/next.config.mjs
@@ -1,1 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+};
+
+export default withPWA({
+  ...nextConfig,
+});
 
